a10140e65d3d0f76fc0588a12bf9506f24c6a611
Jonas Peters <jopeters@c2s16d98.42wolfsburg.de>
2023-11-10 10:40:25 +0100

    basic prompt ok with readline

--- testprograms/readline/main.c
+++ testprograms/readline/main.c
@@ -6,7 +6,7 @@
 /*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 11:48:54 by jopeters          #+#    #+#             */
-/*   Updated: 2023/11/09 12:03:07 by jopeters         ###   ########.fr       */
+/*   Updated: 2023/11/10 10:39:48 by jopeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,59 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 
+void	c_red(void)
+{
+	printf("\033[0;31m");
+}
+
+void	c_green(void)
+{
+	printf("\033[0;32m");
+}
+
+void	c_blue(void)
+{
+	printf("\033[0;34m");
+}
+
+void	c_purple(void)
+{
+	printf("\033[0;35m");
+}
+
+void	c_yellow(void)
+{
+	printf("\033[0;33m");
+}
+
+void	c_cyan(void)
+{
+	printf("\033[0;36m");
+}
+
+void	c_reset(void)
+{
+	printf("\033[0m");
+}
+
+
 int main(void)
 {
     char *input;
-
-    input = readline("Enter your name: ");
-
-    printf("Hello, %s!\n", input);
-
+    printf("\e[1;1H\e[2J");
+    while(1)
+    {
+        c_red();printf("minihell");
+        c_yellow();printf(">>");
+        
+        c_cyan();
+        input = readline("");
+        
+        c_green();
+        printf("|%s|\n", input);
+    }
+    
     free(input);
-
     return 0;
 }
 
