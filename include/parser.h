b569d1c7ffd1684bb48ea6bd4fe5e6e5126ed608
jonaspeters85 <jonaspeters8526@gmail.com>
2023-12-08 13:55:19 +0100

    t_redir rype is enum

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 16:10:30 by sbalk             #+#    #+#             */
-/*   Updated: 2023/12/07 17:08:51 by jonas            ###   ########.fr       */
+/*   Updated: 2023/12/08 13:14:40 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 redirections in a linked list*/
 typedef struct s_redir
 {
-	unsigned int	type;			// JJJ warum nicht t_token_type ?
+	t_token_type	type;			// JJJ warum nicht t_token_type ?
 	char			*target;
 	struct s_redir	*next;
 }				t_redir;
